ede80af66385b39a86153761e3a81e93433c1420
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-03-16 11:29:23 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ const reviewsRouter = require('./reviews/reviews.router')
 const theatersRouter = require('./theaters/theaters.router')
 const notFound = require('./errors/notFound');
 const errorHandler = require('./errors/errorHandler');
+const cors = require('cors')
 
 
 
 
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/movies', moviesRouter)
 app.use('/theaters',theatersRouter)
