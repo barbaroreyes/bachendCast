e3d1509e6b03fc2b7f74053a86632e7c89d2cdf9
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-03-16 13:26:19 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const moviesRouter = require('./movies/movies.router');
+const cors = require('cors');
 const reviewsRouter = require('./reviews/reviews.router')
 const theatersRouter = require('./theaters/theaters.router')
 const notFound = require('./errors/notFound');
 const errorHandler = require('./errors/errorHandler');
-const cors = require('cors')
 
 
 
 
 
-app.use(cors({origin:'*'}))
+
+app.use(cors())
+// app.use(function(req,res,next){
+//     if(res.method === "OPTIONS")
+
+//     res.send(200) 
+
+//     else next()
+// })
 app.use(express.json())
 app.use('/movies', moviesRouter)
 app.use('/theaters',theatersRouter)
